47fd6788c85b24f18f4d02a4d4a79473dbdbfbd1
anfelopera <anfelopera@utp.edu.co>
2023-12-21 01:54:20 +0100

    tuning param0 and optim for final results

--- codes/GP_samples_random_sampling_time_varying_full.py
+++ codes/GP_samples_random_sampling_time_varying_full.py
@@ -18,29 +18,30 @@ def partition(t,X):
     return 1
 
 #Sampling parameters
-nb_reps = 10                        # np of random replicates
-n = 100                             # nb of evaluations of the functional inputs
-N = np.array([50, 100])             # nb of basis functions used in the approximation
+nb_reps = 1000                      # np of random replicates
+n = 20                             # nb of evaluations of the functional inputs
+N = np.array([160])        # nb of basis functions used in the approximation
 d = 500                             # nb of points to approximate the L2 norm
-param0 = np.array([2, 1])           # GP covariance parameters
+param0 = np.array([1., 0.5])        # GP covariance parameters
 param_init = np.array([0.2, 0.2])   # GP covariance parameters
 param_lb = np.array([1e-1, 1e-1])   # parameters' lower bound
-param_ub = np.array([3., 2.])       # parameters' upper bound
+param_ub = np.array([10., 10.])       # parameters' upper bound
 jitter = 1e-12                      # jitter to ensure numerical stability for inversions
-multistart = 1                      # nb of multistarts
+multistart = 5                      # nb of multistarts
 T = np.linspace(0, 1, d)            # integrating variable
 
 print_landscape = False # to plot the log-likelihood landscapes
-verbose = True # to print the optimal parameters per iterations
+verbose = False         # to print the optimal parameters per iterations
 
-expName = "./data_samples/GP_samples_rnd_sampling_time_vary_" 
-
+expName = "./data_samples/GP_samples_rnd_sampling_time_vary/GP_samples_rnd_sampling_time_vary_" 
+print("n = ", n)
 for k in range(np.size(N)):
     print("N = ", N[k])
-    results = np.zeros((nb_reps, 3))
+    results = np.zeros((nb_reps, 4))
 
     for l in range(nb_reps):
-        print("Replicate:", l, end = " ")
+        if l % 100 == 0:
+            print("Replicate:", l)
         # Time varying Samples
         np.random.seed(k+l)
         U = np.random.rand(n,2)
@@ -54,16 +55,15 @@ for k in range(np.size(N)):
         ## Generating GP samples
         distf = ker.dmatrix(f) # distances between functional inputs
         K0 = ker.kernel(param0, ker.dmatrix(f)) # covariance matrix
-        np.random.seed(k+l)
+        np.random.seed(l)
         samples = ker.sample(0, K0, jitter, N=1)[:,0] # matrix with samples
             
         # MLE
         opt_res = maximum_likelihood(param_init, param_lb, param_ub, [0, 1], jitter,
                                      ker.kernel, distf, samples, multistart, opt_method = "Powell")
-        results[l, :] = np.append(opt_res["hat_theta"], np.array(N[k]))
+        results[l, :] = np.append(opt_res["hat_theta"], [n, N[k]])
         if verbose:
             print(results[l, :])
-        print()
         
         ## Computing the log-likelihood landscape
         if print_landscape:
@@ -71,6 +71,6 @@ for k in range(np.size(N)):
             landscape(modified_log_likelihood, ker.kernel, distf, samples,
                       jitter, nbgrid, param0, param_lb, param_ub, opt_res["hat_theta"])
 
-    results = np.vstack((np.append(param0, np.nan), results)) # stacking the ground truth params
-    np.save((expName + "n" + str(n) + "_N" + str(N[k])), results)
+    results = np.vstack((np.append(param0, [np.nan, np.nan]), results)) # stacking the ground truth params
+    np.save((expName  + "nbReps" + str(nb_reps) + "_n" + str(n) + "_N" + str(N[k])), results)
     # results_load = np.load((expName + "N" + str(N[k]) + ".npy"))
--- codes/display.py
+++ codes/display.py
@@ -5,55 +5,93 @@ import pandas as pd
 import numpy as np
 import os
 
-mpl.rc('text', usetex = True) #needed for TeX support in graphs
-sns.set_theme(style="ticks") #Seaborn style - not needed ?
+mpl.rc('text', usetex = True)   # needed for TeX support in graphs
+sns.set_theme(style = "ticks")  # Seaborn style - not needed ?
+palette = sns.color_palette()   # To use the default color palette
 
 #os.chdir("./data_samples") #simulation within this folder must be stored as "GP_samples_Nk.npy" with k an integer
 #and only contain simulation results for one type (either fixed sampling points, random but time independent sampling points,
 # time varying sampling points or Bernstein)
-data_folder = "./data_samples/"
+
+dir_names = os.listdir("./data_samples/")
+
+expIdx = 3
+data_folder = "./data_samples/" + dir_names[expIdx] + "/"
 files = os.listdir(data_folder)
 
 data = []
-
-for s in files: #loads the simulation results
-    N = (s.split("_"))[-1][:-4][1:] #eliminates the part 'GP_samples_N' as well as the extension   
+for s in files: # loads the simulation results
+    N = (s.split("_"))[-1][:-4][1:] # eliminates the part 'GP_samples_N' as well as the extension   
+    n = (s.split("_"))[-2][1:]
     data = data + [np.load(data_folder + s)]
 data = np.concatenate(data)
+df = pd.DataFrame(data, columns=['theta_1', 'theta_2', 'n', 'N']) #dataframe storing all the simulation result
 
-df = pd.DataFrame(data, columns=['theta_1', 'theta_2', 'N']) #dataframe storing all the simulation result
-
-for i,N in enumerate(df['N'].unique()): #for each value of N, we plot a boxplot for both theta_1 and theta_2 and we save it
-    plt.figure()
-    plt.title('N = '+str(N))
-    ax = sns.boxplot((df.loc[df['N'] == N])[['theta_1','theta_2']], width=.15, whis=(10,90))
-    ax.set_xticklabels([r'$\theta _1$',r'$\theta _2$'])
-    ax.set_xlabel("Covariance parameters")
-    ax.set_ylabel("Estimated paramaters")
-    ax.yaxis.grid('True')
-    ax.axhline(y = 0.7,xmin = 0, xmax = 1, ls='--', c='black',lw=1)
-    ax.axhline(y = 1,xmin = 0, xmax = 1, ls='--', c='black',lw=1)
-    #plt.savefig('boxplot_N_'+str(N)+'.png')
-    plt.show()
+nlist = np.sort(df['n'].dropna().unique())
+Nlist = np.sort(df['N'].dropna().unique())
+for j,n in enumerate(nlist): # for each value of n and N, we plot a boxplot for both theta_1 and theta_2 and we save it   
+    for i,N in enumerate(Nlist): 
+        df_N = df.loc[(df['n'] == n) & (df['N'] == N)]        
+        # df_N = df.loc[(df['n'] == n) & (df['N'] == N) & (df['theta_1'] <= 4)]
+        plt.figure()
+        plt.title(r'$n = ' + str(int(n)) +  ', N = ' + str(int(N)) + '$')
+        ax = sns.boxplot(df_N[['theta_1','theta_2']], width=.15, whis=(5,95))
+        ax.set_xticklabels([r'$\theta _1$',r'$\theta _2$'])
+        # ax.set_xlabel("Covariance parameters")
+        # ax.set_ylabel("Estimated paramaters")
+        ax.set_ylim([0, 4])
+        ax.yaxis.grid('True')
+        ax.axhline(y = data[0, 0], xmin = 0.1, xmax = 0.4, ls='--', c='red', lw = 1.5)
+        ax.axhline(y = data[0, 1], xmin = 0.6, xmax = 0.9, ls='--', c='red', lw = 1.5)
+        #plt.savefig('boxplot_n' + n + '_N' + str(N) + '.png')
+        plt.show()
     
-for i,N in enumerate(df['N'].unique()):#for each value of N, we plot a histogram for both theta_1 and theta_2 and we save it
-    plt.figure()
-    plt.title('N = '+str(N))
-    ax = sns.histplot((df.loc[df['N'] == N])['theta_1'])
-    ax.set_xlabel(r'$\theta_1$')
-    ax.set_ylabel("Frequency density")
-    ax.axvline(x = 2, ymin = 0, ymax = 1, ls='--', c='black',lw=1) 
-    #plt.savefig('histplot_theta_1_N_'+str(N)+'.png')
-    plt.show()
+for j,n in enumerate(nlist): # for each value of n and N, we plot a boxplot for both theta_1 and theta_2 and we save it   
+    for i,N in enumerate(Nlist): 
+        df_N = df.loc[(df['n'] == n) & (df['N'] == N)]
+        # df_N = df.loc[(df['n'] == n) & (df['N'] == N) & (df['theta_1'] <= 4)]
+        median_vals = df_N.median()
     
-    plt.figure()
-    plt.title('N = '+str(N))
-    ax = sns.histplot((df.loc[df['N'] == N])['theta_2'])
-    ax.set_xlabel(r'$\theta_2$')
-    ax.set_ylabel("Frequency density")
-    ax.axvline(x = 1, ymin = 0, ymax = 1, ls='--', c='black',lw=1) 
-    #plt.savefig('histplot_theta_2_N_'+str(N)+'.png')
-    plt.show()
+        plt.figure()
+        plt.title(r'$n = ' + str(int(n)) +  ', N = ' + str(int(N)) + '$')
+        # ax = sns.histplot((df.loc[df['N'] == N])['theta_1'])
+        ax = sns.kdeplot(data = df_N, x = "theta_1", fill = True, alpha = 0.3, lw = 1.5)
+        ax.axvline(x = median_vals[0], ymin = 0, ymax = 1, ls = '--', lw = 1.5) 
+        # sns.kdeplot(data = df_N, color = palette[1], fill = True, alpha = 0.3, lw = 1.5)
+        ax.axvline(x = data[0, 0], ymin = 0, ymax = 1, color = palette[1], lw = 1.5) 
+        ax.set_xlim([-2, 4])
+        ax.set_xlabel(r'$\theta_1$')
+        ax.set_ylabel("")
+        #plt.savefig('kdeplot_theta_1_n' + n + '_N' + str(N) + '.png')
+        plt.show()
+    
+        plt.figure()
+        plt.title(r'$n = ' + str(int(n)) +  ', N = ' + str(int(N)) + '$')
+        # ax = sns.histplot((df.loc[df['N'] == N])['theta_1'])
+        ax = sns.kdeplot(data = df_N, x = "theta_2", fill = True, alpha = 0.3, lw = 1.5)
+        ax.axvline(x = median_vals[1], ymin = 0, ymax = 1, ls='--', lw = 1.5) 
+        # sns.kdeplot(data = df_N, color = palette[1], fill = True, alpha = 0.3, lw = 1.5)
+        ax.axvline(x = data[0, 1], ymin = 0, ymax = 1, color = palette[1], lw = 1.5) 
+        ax.set_xlim([0, 1])
+        ax.set_xlabel(r'$\theta_2$')
+        ax.set_ylabel("")
+        #plt.savefig('kdeplot_theta_2_n' + n + '_N' + str(N) + '.png')
+        plt.show()
+    
+        # plt.figure()
+        # plt.title('N = ' + str(N))
+        # # ax = sns.histplot((df.loc[df['N'] == N])['theta_1'])
+        # ax = sns.kdeplot(data = df_N, x = "theta_1", fill = True, alpha = 0.3, lw = 1.5)
+        # ax.axvline(x = median_vals[0], ymin = 0, ymax = 1, ls = '--', lw = 1.5) 
+        # ax.axvline(x = data[0, 0], ymin = 0, ymax = 1, color = palette[2], lw = 1.5) 
+        # sns.kdeplot(data = df_N, x = "theta_2", color = palette[1], fill = True, alpha = 0.3, lw = 1.5)
+        # ax.axvline(x = median_vals[1], ymin = 0, ymax = 1, ls = '--', color = palette[1], lw = 1.5) 
+        # ax.axvline(x = data[0, 1], ymin = 0, ymax = 1, color = palette[3], lw = 1.5) 
+        # ax.set_xlabel(r'$\theta$')
+        # ax.set_ylabel("")
+        #plt.savefig('kdeplot_theta_n' + n + '_N' + str(N) + '.png')
+        # plt.show()
+    
     
 
         
