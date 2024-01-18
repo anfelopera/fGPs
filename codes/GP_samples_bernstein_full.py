asymptotic covariance matrix + fixing script

codes/GP_samples_bernstein_full.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import comb
+from scipy.linalg import sqrtm
 
 from likelihood import *
 import kernel as ker
@@ -29,7 +30,7 @@ expName = "./data_samples/GP_samples_Bernstein/GP_samples_Bernstein_"
 combination_coeff = [[comb(i, k) for k in range(i+1)] for i in N]
 print("n = ", n)
 for k in range(np.size(N)):
-    results = np.zeros((nb_reps, 4))
+    results = np.zeros((nb_reps, 8))
     print("N = ", N[k])
     for l in range(nb_reps):
         if l % 100 == 0:
@@ -46,11 +47,12 @@ for k in range(np.size(N)):
         K0 = ker.kernel(param0, ker.dmatrix(f)) # covariance matrix
         np.random.seed(l)
         samples = ker.sample(0, K0, jitter, N=1)[:,0] # matrix with samples
+        M = np.sqrt(n)*np.real(sqrtm(ker.acov(2,f,0.001,param0)))
             
         # MLE
         opt_res = maximum_likelihood(param_init, param_lb, param_ub, [0, 1], jitter,
                                      ker.kernel, distf, samples, multistart, opt_method = "Powell")
-        results[l, :] = np.append(opt_res["hat_theta"], [n, N[k]])
+        results[l, :] = np.append(opt_res["hat_theta"], [M[0,0],M[0,1],M[1,0],M[1,1],n, N[k]])
         if verbose:
             print(results[l, :])
         
@@ -60,6 +62,6 @@ for k in range(np.size(N)):
             landscape(modified_log_likelihood, ker.kernel, distf, samples,
                       jitter, nbgrid, param0, param_lb, param_ub, opt_res["hat_theta"])
 
-    results = np.vstack((np.append(param0, [np.nan, np.nan]), results)) # stacking the ground truth params
+    results = np.vstack((np.append(param0, [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]), results)) # stacking the ground truth params
     np.save((expName  + "nbReps" + str(nb_reps) + "_n" + str(n) + "_N" + str(N[k])), results)
     # results_load = np.load((expName + "N" + str(N[k]) + ".npy"))

codes/GP_samples_full.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.linalg import sqrtm
 
 from likelihood import *
 from utils import *
@@ -7,7 +8,7 @@ import kernel as ker
 
 #Sampling parameters
 nb_reps = 1000                      # np of random replicates
-n = 10                              # nb of evaluations of the functional inputs
+n = 80                              # nb of evaluations of the functional inputs
 N = np.array([10, 20, 40, 80, 160]) # nb of basis functions used in the approximation
 d = 500                             # nb of points to approximate the L2 norm
 param0 = np.array([1., 0.5])        # GP covariance parameters
@@ -24,7 +25,7 @@ expName = "./data_samples/GP_samples/GP_samples_"
 print("n = ", n)
 for k in range(np.size(N)):
     print("N = ", N[k])
-    results = np.zeros((nb_reps, 4))
+    results = np.zeros((nb_reps, 8))
 
     for l in range(nb_reps):
         if l % 100 == 0:
@@ -42,11 +43,12 @@ for k in range(np.size(N)):
         K0 = ker.kernel(param0, ker.dmatrix(f)) # covariance matrix
         np.random.seed(l)
         samples = ker.sample(0, K0, jitter, N=1)[:,0] # matrix with samples
+        M = np.sqrt(n)*np.real(sqrtm(ker.acov(2,f,0.0001,param0)))
         
         # MLE
         opt_res = maximum_likelihood(param_init, param_lb, param_ub, [0, 1], jitter,
                                      ker.kernel, distf, samples, multistart, opt_method = "Powell")
-        results[l, :] = np.append(opt_res["hat_theta"], [n, N[k]])
+        results[l, :] = np.append(opt_res["hat_theta"], [M[0,0],M[0,1],M[1,0],M[1,1],n, N[k]])
         if verbose:
             print(results[l, :])
         
@@ -56,7 +58,7 @@ for k in range(np.size(N)):
             landscape(modified_log_likelihood, ker.kernel, distf, samples,
                       jitter, nbgrid, param0, param_lb, param_ub, opt_res["hat_theta"])
     
-    results = np.vstack((np.append(param0, [np.nan, np.nan]), results)) # stacking the ground truth params
+    results = np.vstack((np.append(param0, [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]), results)) # stacking the ground truth params
     np.save((expName  + "nbReps" + str(nb_reps) + "_n" + str(n) + "_N" + str(N[k])), results)
     # results_load = np.load((expName + "N" + str(N[k]) + ".npy"))
     

codes/display.py
@@ -15,8 +15,9 @@ palette = sns.color_palette()   # To use the default color palette
 
 dir_names = os.listdir("./data_samples/")
 
-expIdx = 3
+expIdx = 0
 data_folder = "./data_samples/" + dir_names[expIdx] + "/"
+print(data_folder)
 files = os.listdir(data_folder)
 
 data = []
@@ -25,7 +26,9 @@ for s in files: # loads the simulation results
     n = (s.split("_"))[-2][1:]
     data = data + [np.load(data_folder + s)]
 data = np.concatenate(data)
-df = pd.DataFrame(data, columns=['theta_1', 'theta_2', 'n', 'N']) #dataframe storing all the simulation result
+df = pd.DataFrame(data, columns=['theta_1', 'theta_2', 'M_11', 'M_12', 'M_21', 'M_22', 'n', 'N']) #dataframe storing all the simulation result
+df['renorm_diff_1'] = [(d[2]*(d[0]-data[0,0]) + d[4]*(d[1]-data[0,1])) for d in df.to_numpy()]
+df['renorm_diff_2'] = [(d[3]*(d[0]-data[0,0]) + d[5]*(d[1]-data[0,1])) for d in df.to_numpy()]
 
 nlist = np.sort(df['n'].dropna().unique())
 Nlist = np.sort(df['N'].dropna().unique())
@@ -57,12 +60,12 @@ for j,n in enumerate(nlist): # for each value of n and N, we plot a boxplot for 
         plt.figure()
         plt.title(r'$n = ' + str(int(n)) +  ', N = ' + str(int(N)) + '$')
         # ax = sns.histplot((df.loc[df['N'] == N])['theta_1'])
-        ax = sns.kdeplot(data = df_N, x = "theta_1", fill = True, alpha = 0.3, lw = 1.5)
-        ax.axvline(x = median_vals[0], ymin = 0, ymax = 1, ls = '--', lw = 1.5) 
+        ax = sns.kdeplot(data = df_N, x = "renorm_diff_1", fill = True, alpha = 0.3, lw = 1.5)
+        ax.axvline(x = median_vals[-2], ymin = 0, ymax = 1, ls = '--', lw = 1.5) 
         # sns.kdeplot(data = df_N, color = palette[1], fill = True, alpha = 0.3, lw = 1.5)
-        ax.axvline(x = data[0, 0], ymin = 0, ymax = 1, color = palette[1], lw = 1.5) 
-        ax.set_xlim([-2, 4])
-        ax.set_xlabel(r'$\theta_1$')
+        ax.axvline(x = 0, ymin = 0, ymax = 1, color = palette[1], lw = 1.5) 
+        ax.set_xlim([-15, 15])
+        ax.set_xlabel(r'$\sqrt{n}M^{1/2}(\widehat{\theta_1}-\theta_1)$')
         ax.set_ylabel("")
         #plt.savefig('kdeplot_theta_1_n' + n + '_N' + str(N) + '.png')
         plt.show()
@@ -70,12 +73,12 @@ for j,n in enumerate(nlist): # for each value of n and N, we plot a boxplot for 
         plt.figure()
         plt.title(r'$n = ' + str(int(n)) +  ', N = ' + str(int(N)) + '$')
         # ax = sns.histplot((df.loc[df['N'] == N])['theta_1'])
-        ax = sns.kdeplot(data = df_N, x = "theta_2", fill = True, alpha = 0.3, lw = 1.5)
-        ax.axvline(x = median_vals[1], ymin = 0, ymax = 1, ls='--', lw = 1.5) 
+        ax = sns.kdeplot(data = df_N, x = "renorm_diff_2", fill = True, alpha = 0.3, lw = 1.5)
+        ax.axvline(x = median_vals[-1], ymin = 0, ymax = 1, ls='--', lw = 1.5) 
         # sns.kdeplot(data = df_N, color = palette[1], fill = True, alpha = 0.3, lw = 1.5)
-        ax.axvline(x = data[0, 1], ymin = 0, ymax = 1, color = palette[1], lw = 1.5) 
-        ax.set_xlim([0, 1])
-        ax.set_xlabel(r'$\theta_2$')
+        ax.axvline(x = 0, ymin = 0, ymax = 1, color = palette[1], lw = 1.5) 
+        ax.set_xlim([-15, 15])
+        ax.set_xlabel(r'$\sqrt{n}M^{1/2}(\widehat{\theta_2}-\theta_2)$')
         ax.set_ylabel("")
         #plt.savefig('kdeplot_theta_2_n' + n + '_N' + str(N) + '.png')
         plt.show()