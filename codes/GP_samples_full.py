correcting estimation variance and improving the computation of M

codes/GP_samples_full.py
@@ -8,7 +8,7 @@ import kernel as ker
 
 #Sampling parameters
 nb_reps = 1000                      # np of random replicates
-n = 80                              # nb of evaluations of the functional inputs
+n = 160                              # nb of evaluations of the functional inputs
 N = np.array([10, 20, 40, 80, 160]) # nb of basis functions used in the approximation
 d = 500                             # nb of points to approximate the L2 norm
 param0 = np.array([1., 0.5])        # GP covariance parameters
@@ -43,8 +43,9 @@ for k in range(np.size(N)):
         K0 = ker.kernel(param0, ker.dmatrix(f)) # covariance matrix
         np.random.seed(l)
         samples = ker.sample(0, K0, jitter, N=1)[:,0] # matrix with samples
-        M = np.sqrt(n)*np.real(sqrtm(ker.acov(2,f,0.0001,param0)))
-        
+        # M = np.sqrt(n)*np.real(sqrtm(ker.acov(2,f,0.0001,param0)))
+        M = np.real(sqrtm(ker.acov2(2, f, param0)))
+
         # MLE
         opt_res = maximum_likelihood(param_init, param_lb, param_ub, [0, 1], jitter,
                                      ker.kernel, distf, samples, multistart, opt_method = "Powell")

codes/fix.py
@@ -5,21 +5,31 @@ import kernel as ker
 
 #Sampling parameters
 nb_reps = 1000                      # np of random replicates
-tab_n = [10,20,40,80,160]           # nb of evaluations of the functional inputs
-N = np.array([10,20,40,80,160])     # nb of basis functions used in the approximation
+tab_n = [40]       # nb of evaluations of the functional inputs
+N = np.array([160]) # nb of basis functions used in the approximation
 param0 = np.array([1., 0.5])        # GP covariance parameters
 jitter = 1e-12                      # jitter to ensure numerical stability for inversions
 
-
-expName = "./data_samples/GP_samples/GP_samples_" 
-
+expIdx = 0
+if expIdx == 0:
+    expName = "./data_samples/GP_samples/GP_samples_" 
+elif expIdx == 1:
+    expName = "./data_samples/GP_samples_Bernstein/GP_samples_Bernstein_" 
+elif expIdx == 2:
+    expName = "./data_samples/GP_samples_rnd_sampling_time_indep/GP_samples_rnd_sampling_time_indep_" 
+elif expIdx == 3:
+    expName = "./data_samples/GP_samples_rnd_sampling_time_vary/GP_samples_rnd_sampling_time_vary_" 
 
 for n in tab_n:
     print("n = ", n)
     for k in range(np.size(N)):
         print("N = ", N[k])
         new_results = np.zeros((nb_reps+1, 8))
-    
+        param0[0] = param0[0]**2
+        new_results[0,:] = np.append(param0, [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan])
+        results = np.load((expName  + "nbReps" + str(nb_reps) + "_n" + str(n) + "_N" + str(N[k])+ ".npy"))
+        results[:,0] = results[:,0]**2
+        
         for l in range(nb_reps):
             if l % 100 == 0:
                 print("Replicate:", l)
@@ -33,10 +43,12 @@ for n in tab_n:
         
             ## Generating covariance matrices
             distf = ker.dmatrix(f) # distances between functional inputs
-            M = np.sqrt(n)*np.real(sqrtm(ker.acov(2,f,0.0001,param0)))
+            # M = np.real(sqrtm(ker.acov(2,f,0.0001,param0)))
+            M = np.real(sqrtm(ker.acov2(2, f, param0)))
+            #print(M)
             
-            results = np.load((expName  + "nbReps" + str(nb_reps) + "_n" + str(n) + "_N" + str(N[k])+ ".npy"))
-            new_results[0] = np.append(param0, [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan])
-            for i in range(1,new_results.shape[0]):
-                new_results[i] = np.concatenate((results[i,:2],[M[0,0],M[0,1],M[1,0],M[1,1],n, N[k]]))  
-            np.save((expName  + "nbReps" + str(nb_reps) + "_n" + str(n) + "_N" + str(N[k])), new_results)
+            new_results[l+1,:] = np.concatenate((results[l+1,:2],[M[0,0],M[0,1],M[1,0],M[1,1],n, N[k]]))            
+            if l % 100 == 0:
+                np.save((expName  + "nbReps" + str(nb_reps) + "_n" + str(n) + "_N" + str(N[k])), new_results)
+                
+        np.save((expName  + "nbReps" + str(nb_reps) + "_n" + str(n) + "_N" + str(N[k])), new_results)

codes/kernel.py
@@ -1,9 +1,19 @@
 import numpy as np
+from scipy.linalg import cholesky, cho_solve, eig, solve_triangular
 # from scipy.spatial.distance import cdist
 
 # kernel functions
 def kernel(theta, r):
-    return theta[0]**2*np.exp(-np.power(r, 2)/theta[1]**2)
+    # return theta[0]**2*np.exp(-np.power(r, 2)/theta[1]**2)
+    return theta[0]*np.exp(-np.power(r, 2)/theta[1]**2)
+
+
+def dkernel(theta, r, idx_dtheta):
+    if idx_dtheta == 0:
+        # return 2 * kernel(theta, r) / theta[0]
+        return kernel(theta, r) / theta[0]
+    else:
+        return 2 * kernel(theta, r) * np.power(r, 2) / theta[1]**3  
 
 def dmatrix(f):
     n, p = f.shape
@@ -21,6 +31,30 @@ def sample(mu, var, jitter, N):
     return f_post
 
 # computes the asymptotic covariance matrix
+
+def acov2(p, f, param0):
+    dmat = dmatrix(f)           #distance matrix of the sample points 
+    R0 = kernel(param0, dmat)   #R_{theta_0}
+    
+    # computing the Cholesky matrix and the derivatives of R0
+    jitter = 1e-9
+    cR0 = cholesky(R0 + jitter*np.eye(R0.shape[0]), lower = True)
+    dR = [dkernel(param0, dmat, 0), dkernel(param0, dmat, 1)]
+    
+    # Computing M via Cholesky and solve triangular 
+    cR0t = np.transpose(cR0)
+    P = [solve_triangular(cR0, dR[0], lower=True),
+         solve_triangular(cR0, dR[1], lower=True)]
+    Q = [solve_triangular(cR0t, P[0]),
+         solve_triangular(cR0t, P[1])]
+    
+    M = np.zeros((p,p))
+    n = f.shape[0]
+    for i in range(p):
+        for j in range(p):
+            M[i,j] = (1/(2*n))*np.trace(np.matmul(Q[i], Q[j]))
+    return M
+
 def acov(p,f,h,param0):
     M = np.zeros((p,p))
     n = f.shape[0]
@@ -34,9 +68,14 @@ def acov(p,f,h,param0):
 def prodmat(p,i,j,f,h,param0):
     dmat = dmatrix(f)           #distance matrix of the sample points 
     R0 = kernel(param0, dmat)   #R_{theta_0}
-    jitter = 1e-5
+    jitter = 1e-6
 
-    inv = np.linalg.inv(R0 + jitter*np.eye(R0.shape[0]))
+    n = R0.shape[0]
+    # inv2 = np.linalg.inv(R0 + jitter*np.eye(R0.shape[0]))
+    # print(min(eig(R0 + jitter*np.eye(n))[0]))
+    # print(np.max(R0 - np.transpose(R0)))
+    cR0 = cholesky(R0 + jitter*np.eye(n), lower = True)
+    #inv = cho_solve((cR0, True), np.eye(n))
 
     off = np.zeros(p)   
     off[i] = h
@@ -49,4 +88,15 @@ def prodmat(p,i,j,f,h,param0):
     dRi = (Ri - R0)/h #First order derivative in direction i
     dRj = (Rj - R0)/h #First order derivative in direction j
 
-    return np.matmul(inv,np.matmul(dRj,np.matmul(inv,dRi)))
+    # return np.matmul(inv,np.matmul(dRj,np.matmul(inv,dRi)))
+    
+    cR0t = np.transpose(cR0)
+    Pj = solve_triangular(cR0, dRj, lower=True)
+    Pi = solve_triangular(cR0, dRi, lower=True)
+    Oj = solve_triangular(cR0t, Pj)
+    Oi = solve_triangular(cR0t, Pi)
+    Q = np.matmul(Oi, Oj)
+    
+    return(Q)
+    
+    